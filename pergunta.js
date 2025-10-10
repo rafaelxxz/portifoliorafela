0aacafeb4c810f2895cc0d415a31205dd07a7a84
rafaelxxz <r.figueira@escola.pr.gov.br>
2025-10-10 08:04:27 -0300

    56y

---
+++ pergunta.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'programação',
+    'O que é python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da frança?',
+    'A capital da França é paris'
+)
+
+criaCartao(
+    'programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
